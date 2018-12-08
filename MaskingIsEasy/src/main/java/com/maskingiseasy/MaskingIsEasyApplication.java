da8472321eb5d1531e70db81ee0fc260c4934645
Asus <Asus@YELMACI-WORK-PC>
2018-12-08 23:03:35 +0300

    sss

--- MaskingIsEasy/src/main/java/com/maskingiseasy/MaskingIsEasyApplication.java
+++ MaskingIsEasy/src/main/java/com/maskingiseasy/MaskingIsEasyApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MaskingIsEasyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MaskingIsEasyApplication.class, args);
+		SpringApplication.run(MaskingIsEasyApplication.class, args); 
 	}
 }
